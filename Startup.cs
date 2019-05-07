6b0149327838015bc28bacae170ff78ef2e72d11
tac <10121496+asnowcat@users.noreply.github.com>
2019-05-07 22:09:47 +0100

    Remove redundant option (HttpOnly defaults to true)

--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,7 @@ namespace MosaicExercise
             {
                 // Set a short timeout for easy testing.
                 options.IdleTimeout = TimeSpan.FromSeconds(10);
-                options.Cookie.HttpOnly = true;
+
                 // Make the session cookie essential
                 options.Cookie.IsEssential = true;
             });
